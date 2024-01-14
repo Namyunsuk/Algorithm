7ef0823fe53cdfb9858d1f251681b68a78223da2
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2024-01-15 03:42:37 +0900

    [Silver I] Title: 그림, Time: 20 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -12,6 +12,9 @@ int dy[4] = { 1,0,-1,0 };
 
 
 int main(int argc, char** argv) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
     int n, m, val, cnt=0, mx=0;
     queue<pair<int, int>> Q;
 
@@ -22,35 +25,32 @@ int main(int argc, char** argv) {
             arr[i][j] = val;
         }
     }
-
-
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             if (arr[i][j] != 1 || isvisit[i][j] == 1) continue;
-            int  area = 1;
+            int  area = 0;
             isvisit[i][j] = 1;
             Q.push({ i,j });
             cnt += 1;
             while (!Q.empty()) {
-                if (area > mx) mx = area;
+                area++;
                 pair<int, int> cur = Q.front();
                 Q.pop();
                 for (int dir = 0; dir < 4; dir++) {
                     int nx = cur.X + dx[dir];
                     int ny= cur.Y + dy[dir];
-
                     if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
                     if (arr[nx][ny] != 1 || isvisit[nx][ny] == 1) continue;
                     isvisit[nx][ny] = 1;
                     Q.push({ nx,ny });
-                    area++;
+                    
                 }
             }
+            mx = max(mx, area);
         }
     }
     
-    cout << cnt << "\n";
-    cout << mx;
+    cout << cnt << "\n"<<mx;
 
    
     return 0;
