5e32c632c71af8a97acef62ea1c19e165504ccc6
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2024-01-17 14:55:37 +0900

    [Gold V] Title: 적록색약, Time: 0 ms, Memory: 2168 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.cc
+++ 백준/Gold/10026. 적록색약/적록색약.cc
@@ -12,7 +12,7 @@ int dy[4] = { 0,1,0,-1 };
 int n;
 queue < pair<int, int>> Q;
 
-void bfs1(int i, int j, char color) {
+void bfs(int i, int j) {
     isvisit[i][j] = true;
     Q.push({ i,j });
     while (!Q.empty()) {
@@ -22,24 +22,7 @@ void bfs1(int i, int j, char color) {
             int nx = cur.X + dx[dir];
             int ny = cur.Y + dy[dir];
             if (nx < 0 || nx >= n || ny < 0 || ny >= n)continue;
-            if (isvisit[nx][ny] || arr[nx][ny] != color)continue;
-            isvisit[nx][ny] = true;
-            Q.push({ nx,ny });
-        }
-    }
-}
-
-void bfs2(int i, int j) {
-    isvisit[i][j] = true;
-    Q.push({ i,j });
-    while (!Q.empty()) {
-        auto cur = Q.front();
-        Q.pop();
-        for (int dir = 0; dir < 4; dir++) {
-            int nx = cur.X + dx[dir];
-            int ny = cur.Y + dy[dir];
-            if (nx < 0 || nx >= n || ny < 0 || ny >= n)continue;
-            if (isvisit[nx][ny] || arr[nx][ny] == 'B')continue;
+            if (isvisit[nx][ny] || arr[nx][ny] != arr[cur.X][cur.Y])continue;
             isvisit[nx][ny] = true;
             Q.push({ nx,ny });
         }
@@ -57,13 +40,11 @@ int main(int argc, char** argv) {
         cin >> arr[i];
     }
 
-    for (int color : {'R', 'G', 'B'}) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (isvisit[i][j] || arr[i][j] != color)continue;
-                bfs1(i, j, color);
-                result1 += 1;
-            }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (isvisit[i][j])continue;
+            bfs(i, j);
+            result1 += 1;
         }
     }
 
@@ -73,16 +54,16 @@ int main(int argc, char** argv) {
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            if (isvisit[i][j] || arr[i][j] =='B')continue;
-            bfs2(i, j);
-            result2 += 1;
+            if (arr[i][j] == 'G') {
+                arr[i][j] = 'R';
+            }
         }
     }
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            if (isvisit[i][j] || arr[i][j] != 'B')continue;
-            bfs1(i, j, 'B');
+            if (isvisit[i][j])continue;
+            bfs(i, j);
             result2 += 1;
         }
     }
