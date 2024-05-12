0998764db5dfed94a58db00523bdeee9e4276f43
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2024-05-12 23:29:39 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -1,26 +1,21 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(int argc, char** argv) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
-    int arr[26];
-    fill(arr, arr + 26, 0);
+int freq[26];
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  
+  string S;
+  
+  cin>>S;
 
-    string s;
-    cin >> s;
-    
-    for (auto c : s) {
-        arr[c - 'a'] += 1;
-    }
+  for(auto e:S){
+    freq[e-'a']+=1;
+  }
 
-    for (int i = 0; i < 26;i++) {
-        cout << arr[i] << ' ';
-    }
-
-   
-   
-    return 0;
-}
+  for(auto e:freq){
+    cout<<e<<' ';
+  }
+  
+} 
