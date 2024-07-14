1d7048dcb1576ababad169d2de153d0df43105ba
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2024-07-15 01:06:37 +0900

    [Gold V] Title: 괄호의 값, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.cc
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.cc
@@ -1,49 +1,63 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-int main(int argc, char** argv) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+  stack<int> S;
+  string str;
+  int result = 0, mul = 1;
 
-    stack<char> S;
-    string seq;
-    int val=1,result = 0;
+  cin >> str;
 
-
-    cin >> seq;
-    for (int i = 0; i < seq.size(); i++) {
-        if (seq[i] == '(') {
-            val *= 2;
-            S.push(seq[i]);
-        }
-        else if (seq[i] == '[') {
-            val *= 3;
-            S.push(seq[i]);
-        }
-        else if (seq[i] == ')') {
-            if (S.empty() || S.top() != '(') {
-                cout << 0;
-                return 0;
-            }
-            if (seq[i - 1] == '(') result += val;
-            val /= 2;
-            S.pop();
-        }
-        else if (seq[i] == ']') {
-            if (S.empty() || S.top() != '[') {
-                cout << 0;
-                return 0;
-            }
-            if ( seq[i - 1] == '[') result += val;
-            val /= 3;
-            S.pop();
-        }
+  for(int i=0; i<str.size(); i++){
+    if(str[i]=='('){
+      mul*=2;
+      S.push(str[i]);
     }
-    if (!S.empty())cout << 0;
-    else cout << result;
-    
-   
-    return 0;
+    else if(str[i]=='['){
+      mul*=3;
+      S.push(str[i]);
+    }
+    else if(str[i]==')'){
+      if(S.empty()|| S.top()!='('){
+        cout << 0;
+        return 0;
+      }
+      if(i!=0&&str[i-1]=='('){
+        result+=mul;
+        mul/=2;
+        S.pop();
+      }
+      else{
+        mul/=2;
+        S.pop();
+      }
+    }
+    else if(str[i]==']'){
+      if(S.empty()|| S.top()!='['){
+        cout << 0;
+        return 0;
+      }
+      if(i!=0&&str[i-1]=='['){
+        result+=mul;
+        mul/=3;
+        S.pop();
+      }
+      else{
+        mul/=3;
+        S.pop();
+      }
+    }
+  }
+
+  if(S.empty()){
+    cout << result;
+  }
+  else{
+    cout << 0;
+  }
 }
+
+
