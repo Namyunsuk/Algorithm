e0fd62ac56011c8cbdf5ad75cc62972cd644e56d
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2024-07-19 22:16:50 +0900

    [Gold V] Title: 토마토, Time: 80 ms, Memory: 10208 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,58 +1,52 @@
 #include <bits/stdc++.h>
-
-#define x first
-#define y second
-
 using namespace std;
 
-int arr[1000][1000];
-int dx[4] = { 1,0,-1,0 };
-int dy[4] = { 0,1,0,-1 };
+#define X first
+#define Y second
 
+int n,m;
+int board[1000][1000];
 
-int main(int argc, char** argv) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
 
-    queue<pair<int, int>> Q;
-    int n, m;
-    cin >> m >> n;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cin >> arr[i][j];
-            if (arr[i][j] == 1) Q.push({ i,j });
-        }
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int ans = -1;
+  queue<pair<int, int>> Q;
+
+  cin >> m>>n;
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      cin >> board[i][j];
+      if(board[i][j] == 1) Q.push({i,j});
     }
+  }
 
-    while (!Q.empty()) {
-        auto cur = Q.front();
-        Q.pop();
-        for (int i = 0; i < 4; i++) {
-            int nx = cur.x + dx[i];
-            int ny = cur.y + dy[i];
-            if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-            if (arr[nx][ny] != 0) continue;
-            arr[nx][ny] = arr[cur.x][cur.y] + 1;
-            Q.push({ nx,ny });
-        }
+  while(!Q.empty()){
+    pair<int, int> cur = Q.front(); Q.pop();
+    for(int i=0;i<4;i++){
+      int nx = cur.X + dx[i];
+      int ny = cur.Y + dy[i];
+
+      if(nx<0 || nx>=n || ny<0 || ny>=m) continue;
+      if(board[nx][ny]!=0) continue;
+      board[nx][ny] = board[cur.X][cur.Y] + 1;
+      Q.push({nx, ny});
     }
+  }
 
-    int mx = -1;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (arr[i][j] == 0) {
-                cout << -1;
-                return 0;
-            }
-            mx = max(mx, arr[i][j]);
-        }
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(board[i][j]==0){
+        cout << -1;
+        return 0;
+      }
+      ans = max(ans, board[i][j]);
     }
-    
-    if (mx == 1) cout << 0;
-    else cout << mx-1;
-
-
-   
-   
-    return 0;
+  }
+  if(ans == 1) cout << 0;
+  else cout << ans-1;
 }
