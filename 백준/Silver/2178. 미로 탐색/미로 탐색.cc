3ab3d4bed46b48b9a683f92953db90cd6b4f147b
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2024-07-19 20:33:03 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2200 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,45 +1,49 @@
 #include <bits/stdc++.h>
+using namespace std;
 
 #define X first
 #define Y second
 
-using namespace std;
-
-string arr[100];
+int n,m;
+string board[100];
 int dist[100][100];
+
 int dx[4] = {1,0,-1,0};
-int dy[4] = {0,-1,0,1};
+int dy[4] = {0,1,0,-1};
 
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-int main(int argc, char** argv) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+  queue<pair<int, int>> Q;
 
-    int n, m, num;
-    queue<pair<int, int>> Q;
-    
-    cin >> n >> m;
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+  cin >>n>>m;
+
+  for(int i=0; i<n; i++){
+    cin >> board[i];
+  }
+
+  for(int i=0; i<n;i++){
+    for(int j=0; j<m; j++){
+      dist[i][j] = -1;
     }
+  }
 
-    dist[0][0] = 1;
-    Q.push({0,0});
+  dist[0][0] = 0;
+  Q.push({0,0});
 
-    while (!Q.empty()) {
-        pair<int, int> cur = Q.front();
-        Q.pop();
-        for (int i = 0; i < 4; i++) {
-            int nx = cur.X + dx[i];
-            int ny = cur.Y + dy[i];
-            if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-            if (arr[nx][ny] != '1' || dist[nx][ny] >0) continue;
-            dist[nx][ny] = dist[cur.X][cur.Y] + 1;
-            Q.push({ nx,ny });
-        }
+  while(!Q.empty()){
+    pair<int, int> cur = Q.front(); Q.pop();
+    for(int i=0;i<4;i++){
+      int nx = cur.X+dx[i];
+      int ny = cur.Y+dy[i];
+
+      if(nx<0 || nx>=n || ny<0 || ny>=m) continue;
+      if(dist[nx][ny]>=0 || board[nx][ny]=='0') continue;
+      dist[nx][ny] = dist[cur.X][cur.Y] + 1;
+      Q.push({nx, ny});
     }
+  }
 
-    cout << dist[n - 1][m - 1];
-   
-    return 0;
+  cout << dist[n-1][m-1]+1;
 }
