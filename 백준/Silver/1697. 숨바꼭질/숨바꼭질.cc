104c1361296cca78bd68b3c355a90910012afe54
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2024-07-26 18:33:26 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2544 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -1,39 +1,32 @@
 #include <bits/stdc++.h>
-
-#define x first
-#define y second
-
 using namespace std;
 
-int arr[100001];
+int board[100002];
 
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-int main(int argc, char** argv) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+  queue<int> Q;
+  int n,k;
+  cin >>n >> k;
 
+  fill(board, board+100001, -1);
 
-    queue<int> Q;
-    int n, k;
-    fill(arr, arr + 100001, -1);
+  board[n] = 0;
+  Q.push(n);
 
-    cin >> n >> k;
-    arr[n] = 0;
-    Q.push(n);
+  while(board[k]==-1){
+    auto cur = Q.front(); Q.pop();
+    for(auto nx: {cur-1, cur+1, cur*2}){
 
-    while (arr[k]==-1) {
-        int cur = Q.front();
-        Q.pop();
-        for (int nx:{cur-1,cur+1,cur*2}) {
-            if (nx < 0 || nx > 100000) continue;
-            if (arr[nx] != -1) continue;
-            arr[nx] = arr[cur] + 1;
-            Q.push(nx);
-        }
+      if(nx<0 || nx>100000) continue;
+      if(board[nx] != -1) continue;
+      board[nx] = board[cur] + 1;
+      Q.push(nx);
     }
-   
-    cout << arr[k];
-   
-   
-    return 0;
+  }
+
+  cout << board[k];
+
 }
