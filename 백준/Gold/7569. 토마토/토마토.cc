6792ce6c50631fc330acff7622c7aff598b21ae5
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2024-07-30 02:17:25 +0900

    [Gold V] Title: 토마토, Time: 96 ms, Memory: 7636 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.cc
+++ 백준/Gold/7569. 토마토/토마토.cc
@@ -1,74 +1,66 @@
 #include <bits/stdc++.h>
-
-#define x first
-#define y second
-#define y third
-
 using namespace std;
 
-int arr[100][100][100];
-int dist[100][100][100];
+int board[101][101][101];
 
-int dx[6] = { 1,-1,0,0,0,0 };
-int dy[6] = { 0,0,1,-1,0,0 };
-int dz[6] = { 0,0,0,0,1,-1 };
+int dx[6] = {0, -1, 0 ,1 , 0, 0};
+int dy[6] = {-1, 0, 1, 0, 0, 0};
+int dz[6] = {0, 0, 0 , 0, -1, 1};
 
-int main(int argc, char** argv) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+int ans = 0;
 
-    int m, n, h;
-    queue<tuple<int, int, int>> Q;
-    cin >> m >> n >> h;
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-    for (int i = 0; i < h; i++) {
-        for (int j = 0; j < n; j++) {
-            fill(dist[i][j], dist[i][j] + m, -1);
+  int n, m, h;
+  queue<tuple<int, int, int>> Q;
+
+  cin >> m>>n>>h;
+
+  for(int i=0; i<h; i++){
+    for(int j=0; j<n; j++){
+      for(int k=0; k<m; k++){
+        cin>> board[i][j][k];
+
+        if(board[i][j][k]== 1){
+          Q.push({i, j, k});
         }
+      }
     }
+  }
 
-    for (int i = 0; i < h; i++) {
-        for (int j = 0; j < n; j++) {
-            for (int k = 0; k < m; k++) {
-                cin >> arr[i][j][k];
-                if (arr[i][j][k] == 1) {
-                    dist[i][j][k] = 0;
-                    Q.push({ i,j,k });
-                }
-            }
+  while(!Q.empty()){
+    auto cur = Q.front(); Q.pop();
+    int curX, curY, curZ;    
+    tie(curZ, curX, curY) = cur;
+
+    for(int i=0; i<6; i++){
+      int nx = curX + dx[i];
+      int ny = curY + dy[i];
+      int nz = curZ + dz[i];
+
+      if(nx<0|| nx>=n|| ny<0|| ny>=m|| nz<0|| nz>=h) continue;
+      if(board[nz][nx][ny]!=0) continue;
+      board[nz][nx][ny] = board[curZ][curX][curY]+1;
+      Q.push({nz, nx, ny});
+    }
+  }
+
+  for(int i=0; i<h; i++){
+    for(int j=0; j<n; j++){
+      for(int k=0; k<m; k++){
+        if(board[i][j][k]==-1) continue;
+        if(board[i][j][k]==0){
+          cout << -1;
+          return 0;
         }
+        ans = max(ans, board[i][j][k]);
+      }
     }
+  }
 
-    while (!Q.empty()) {
-        int curx, cury, curz;
-        auto cur = Q.front();
-        Q.pop();
-        tie(curz, curx, cury) = cur;
-        for (int i = 0; i < 6; i++) {
-            int nx = curx + dx[i];
-            int ny = cury + dy[i];
-            int nz = curz + dz[i];
-            if (nx < 0 || nx >= n || ny < 0 || ny >= m || nz < 0 || nz >= h)continue;
-            if (dist[nz][nx][ny] >= 0 || arr[nz][nx][ny] == -1)continue;
-            dist[nz][nx][ny] = dist[curz][curx][cury] + 1;
-            Q.push({ nz,nx,ny });
-        }
-    }
-
-    int mx = 0;
-    for (int i = 0; i < h; i++) {
-        for (int j = 0; j < n; j++) {
-            for (int k = 0; k < m; k++) {
-                if (arr[i][j][k]!=-1 && dist[i][j][k] == -1) {
-                    cout << -1;
-                    return 0;
-                }
-                mx = max(mx, dist[i][j][k]);
-            }
-        }
-    }
-    
-    cout << mx;
-
-    return 0;
+  cout <<ans-1;
+  
+  
 }
