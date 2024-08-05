43645956d5e89d1e87e94460f6b2e2180010a6c2
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2024-08-05 20:23:42 +0900

    [Gold IV] Title: 불, Time: 60 ms, Memory: 13080 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.cc
+++ 백준/Gold/5427. 불/불.cc
@@ -1,80 +1,90 @@
 #include <bits/stdc++.h>
-
-#define x first
-#define y second
-
 using namespace std;
 
-string arr[1000];
-int dist1[1000][1000];
-int dist2[1000][1000];
+#define X first
+#define Y second
 
-int dx[4] = { 1,0,-1,0 };
-int dy[4] = { 0,1,0,-1 };
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
 
-int main(int argc, char** argv) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+string board[1001];
 
-    int t, n, m;
-    cin >> t;
-    while (t--) {
-        bool escape = false;
-        queue<pair<int, int>> Q1;
-        queue<pair<int, int>> Q2;
-        cin >> m>> n;
-        for (int i = 0; i < n; i++) {
-            fill(dist1[i], dist1[i] + m, -1);
-            fill(dist2[i], dist2[i] + m, -1);
-        }
-        for (int i = 0; i < n; i++) {
-            cin >> arr[i];
-            for (int j = 0; j < m; j++) {
-                if (arr[i][j] == '@') {
-                    dist2[i][j] = 0;
-                    Q2.push({ i,j });
-                }
-                if (arr[i][j] == '*') {
-                    dist1[i][j] = 0;
-                    Q1.push({ i,j });
-                }
-            }
-        }
 
-        while (!Q1.empty()) {
-            auto cur = Q1.front();
-            Q1.pop();
-            for (int i = 0; i < 4; i++) {
-                int nx = cur.x + dx[i];
-                int ny = cur.y + dy[i];
-                if (nx < 0 || nx >= n || ny<0 || ny>=m)continue;
-                if (dist1[nx][ny] >= 0 || arr[nx][ny] == '#')continue;
-                dist1[nx][ny] = dist1[cur.x][cur.y] + 1;
-                Q1.push({ nx,ny });
-            }
-        }
+int vis1[1001][1001];
+int vis2[1001][1001];
 
-        while (!Q2.empty()) {
-            auto cur = Q2.front();
-            Q2.pop();
-            for (int i = 0; i < 4; i++) {
-                int nx = cur.x + dx[i];
-                int ny = cur.y + dy[i];
-                if (nx < 0 || nx >= n || ny<0 || ny>=m) {
-                    cout << dist2[cur.x][cur.y] + 1<<"\n";
-                    escape = true;
-                    break;
-                }
-                if (dist2[nx][ny] >= 0 || arr[nx][ny] == '#')continue;
-                if (dist1[nx][ny]!=-1 && dist1[nx][ny] <= dist2[cur.x][cur.y] + 1)continue;
-                dist2[nx][ny] = dist2[cur.x][cur.y] + 1;
-                Q2.push({ nx,ny });
-            }
-            if (escape)break;
-        }
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-        if(!escape)cout << "IMPOSSIBLE\n";
+  int t, m, n;
+
+  cin >> t;
+
+  while(t--){
+    bool fin = false;
+    queue<pair<int, int>> Q1, Q2; // 1: 불, 2: 상근
+    cin >> n >> m;
+
+    fill(board, board+m, "");
+
+    for(int i=0; i<m; i++){
+      fill(vis1[i], vis1[i]+n, 0);
+      fill(vis2[i], vis2[i]+n, 0);
     }
 
-    return 0;
+    for(int i=0;i<m;i++){ // 상근, 불 초기 위치 세팅
+      cin >> board[i];
+      for(int j=0; j<n;j++){
+        if(board[i][j]=='*'){
+          Q1.push({i, j});
+        }
+        if(board[i][j]=='@'){
+          Q2.push({i, j});
+        }
+      }
+    }
+
+    // 불 탐색
+    while(!Q1.empty()){
+      auto cur = Q1.front(); Q1.pop();
+
+      for(int dir=0; dir<4; dir++){
+        int nx = cur.X + dx[dir];
+        int ny = cur.Y + dy[dir];
+
+        if(nx<0||nx>=m||ny<0||ny>=n) continue; // 벗어난 경우
+        if(board[nx][ny]=='#' || board[nx][ny]=='@') continue; // 벽 또는 상근이인 경우
+        if(vis1[nx][ny]>0) continue; // 이미 방문
+        vis1[nx][ny] = vis1[cur.X][cur.Y] + 1;
+        Q1.push({nx, ny});
+      }
+    }
+    
+    // 상근 탐색
+    while(!Q2.empty()){
+      auto cur = Q2.front(); Q2.pop();
+
+      for(int dir=0; dir<4; dir++){
+        int nx = cur.X + dx[dir];
+        int ny = cur.Y + dy[dir];
+
+        if(nx<0||nx>=m||ny<0||ny>=n){ // 탈출한 경우
+          cout << vis2[cur.X][cur.Y] + 1<<"\n";
+          fin = true;
+          break;
+        }
+        if(vis1[nx][ny]!=0&&(vis1[nx][ny]<= vis2[cur.X][cur.Y] + 1 )) continue;
+        if(board[nx][ny]=='#' || board[nx][ny]=='*') continue; // 벽 또는 상근이인 경우
+        if(vis2[nx][ny]>0) continue; // 이미 방문
+        vis2[nx][ny] = vis2[cur.X][cur.Y] + 1;
+        Q2.push({nx, ny});
+      }
+      if(fin) break;
+    }
+
+    if(!fin){
+      cout << "IMPOSSIBLE\n";
+    }
+  }
 }
