920005e77b5c070a5e49b91e0c8df829f1aa01f5
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2024-09-14 23:31:47 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define X first
+#define Y second
+
+int dp[1002];
+
+int main(void)
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n;
+  cin >> n;
+
+  dp[1] = 1;
+  dp[2] = 2;
+
+  for(int i=3;i<=n;i++){
+    dp[i] = (dp[i-1] + dp[i-2])%10007;
+  }
+  cout << dp[n];
+}
+
+
