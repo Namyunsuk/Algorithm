8fcda62e8d6f3a6a1330a82b7d59e2cb7166bf4e
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2024-10-29 11:31:44 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 84 ms, Memory: 12408 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.kt
@@ -0,0 +1,27 @@
+val dp = Array(102) { Array(11) { 0 } }
+
+fun main() {
+    val n = readln().toInt()
+
+    for (i in 1..9) {
+        dp[1][i] = 1
+    }
+
+    for (i in 2..n) {
+        for (j in 0..9) {
+            if (j != 0) dp[i][j] += dp[i - 1][j - 1]
+            if (j != 9) dp[i][j] += dp[i - 1][j + 1]
+            dp[i][j] %=1000000000
+        }
+    }
+
+    var ans = 0L
+
+    for(i in 0 .. 9){
+        if(n==1 && i==0) continue
+        ans += dp[n][i]
+    }
+
+    println(ans%1000000000)
+
+}
