21d8d027d86f3ef8c053d6abe8c9ee21b864668c
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2024-11-18 23:52:38 +0900

    [Silver I] Title: 포도주 시식, Time: 104 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.kt
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.kt
@@ -1,25 +1,23 @@
-import kotlin.math.*
-
 val dp = Array(10002) { 0 }
 val arr = Array(10002) { 0 }
 
-fun main() {
-    val n = readln().toInt()
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readLine().toInt()
 
     for (i in 1..n) {
-        arr[i] = readln().toInt()
+        arr[i] = readLine().toInt()
+        if (i == 1) {
+            dp[i] = arr[1]
+        } else if (i == 2) {
+            dp[i] = arr[1] + arr[2]
+        } else if (i == 3) {
+            dp[i] = maxOf(dp[2], arr[1] + arr[3], arr[2] + arr[3])
+        }
     }
-
-    dp[1] = arr[1]
-    dp[2] = arr[1] + arr[2]
-    dp[3] = max(max(arr[1] + arr[2], arr[1] + arr[3]), arr[2] + arr[3])
 
     for (i in 4..n) {
-        dp[i] = max(dp[i - 2], dp[i - 3] + arr[i - 1]) + arr[i]
-        dp[i] = max(dp[i - 1], dp[i])
+        dp[i] = maxOf(dp[i - 1], dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i])
     }
 
-    val ans = dp.maxOf { it }
-
-    println(ans)
+    print(dp[n])
 }
