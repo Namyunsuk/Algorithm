e5dc4ed8ad13990c0704c6d41b91c9314e25eaa0
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2024-11-20 23:31:04 +0900

    [Gold IV] Title: N-Queen, Time: 5388 ms, Memory: 13220 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.kt
@@ -0,0 +1,41 @@
+import java.util.*
+
+data class Pos(val x: Int, val y: Int)
+
+lateinit var vis: Array<Int>
+var ans = 0
+
+fun main(): Unit = with(System.`in`.bufferedReader()) {
+    val n = readLine().toInt()
+
+    vis = Array(n) { -1 }
+
+    for (j in 0..<n) {
+        vis[0] = j
+        dfs(1, n)
+    }
+    print(ans)
+}
+
+fun dfs(row: Int, n: Int) {
+    if (row == n) {
+        ans++
+        return
+    }
+
+    for (j in 0..<n) {
+        if (!check(row, j)) continue
+        vis[row] = j
+        dfs(row + 1, n)
+    }
+}
+
+fun check(x: Int, y: Int): Boolean {
+    for (i in 0..<x) {
+        if (vis[i] == y) return false // 위
+        if (x - y == i - vis[i]) return false //좌측 위
+        if (x + y == i + vis[i]) return false //우측 위
+    }
+
+    return true
+}
