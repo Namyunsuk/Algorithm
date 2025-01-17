2e7efa23de5df0402fd7aca3cce6f21c032a44ee
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2025-01-17 17:32:41 +0900

    [Gold IV] Title: 우체국, Time: 656 ms, Memory: 70852 KB -BaekjoonHub

--- 백준/Gold/2141. 우체국/우체국.kt
+++ 백준/Gold/2141. 우체국/우체국.kt
@@ -1,49 +1,25 @@
-val countries = mutableListOf<Country>()
-val nums = mutableListOf<Int>()
-val index = mutableListOf<Int>()
-lateinit var leftSum: Array<Long>
-lateinit var rightSum: Array<Long>
-
 data class Country(val index: Int, val cnt: Int)
+
+val countries = mutableListOf<Country>()
 
 fun main(): Unit = with(System.`in`.bufferedReader()) {
     val n = readLine().toInt()
-    leftSum = Array(n) { 0L }
-    rightSum = Array(n) { 0L }
+    var totalPeople = 0L
     repeat(n) {
         val (i, num) = readLine().split(" ").map { it.toInt() }
+        totalPeople += num
         countries.add(Country(i, num))
     }
     countries.sortWith(compareBy { it.index })
 
-    countries.forEach { country ->
-        index.add(country.index)
-        nums.add(country.cnt)
-    }
+    val mid = (totalPeople+1) / 2
+    var acc = 0L
 
-    leftSum[0] = nums[0].toLong()
-    for (i in 1 until n) {
-        leftSum[i] = leftSum[i - 1] + nums[i]
-    }
-
-    rightSum[n - 1] = nums[n - 1].toLong()
-    for (i in n - 2 downTo 0) {
-        rightSum[i] = rightSum[i + 1] + nums[i]
-    }
-
-    var accSum = 0L
-    for (i in 1 until n) {
-        accSum += leftSum[i - 1]
-    }
-
-    var minSum = accSum
-    var result = n - 1
-    for (i in n - 2 downTo 0) {
-        accSum += (rightSum[i + 1] - leftSum[i])
-        if (accSum <= minSum) {
-            minSum = accSum
-            result = i
+    for (country in countries) {
+        acc += country.cnt
+        if(acc>=mid){
+            print(country.index)
+            break
         }
     }
-    print(index[result])
 }
