312520d619fc42e3dd0d715fa861493d032ad57a
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2025-04-20 02:46:46 +0900

    [level 2] Title: 카펫, Time: 8.05 ms, Memory: 64.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.kt
+++ 프로그래머스/2/42842. 카펫/카펫.kt
@@ -1,38 +1,23 @@
-import kotlin.math.*
-
-val 가로세로모음 = mutableListOf<Pair<Int, Int>>()
-
 class Solution {
     fun solution(brown: Int, yellow: Int): IntArray {
         var answer = mutableListOf<Int>()
         
-        val width = brown + yellow
+        var m = 0
         
-        for(i in 1 .. sqrt(width.toDouble()).toInt()){
-            if(width%i==0){
-                val 가로 = width/i
-                val 세로 = i
-                가로세로모음.add(가로 to 세로)
+        for(n in 1..yellow){
+            if(yellow%n!=0) continue
+            val m = yellow/n
+            
+            if((n+2)*(m+2)==(brown+yellow)){
+                answer.add(n+2)
+                answer.add(m+2)
+                break
             }
         }
         
-        val tmp = 가로세로모음.filter{
-            해결책(it.first, it.second, brown)
-        }
+        answer.sortByDescending{it}
         
-        answer.add(tmp[0].first)
-        answer.add(tmp[0].second)
         
         return answer.toIntArray()
     }
-    
-    fun 해결책(가로: Int, 세로: Int, 갈색수:Int):Boolean{
-        return 가로 + 가로 + (세로-2) + (세로-2) == 갈색수
-    }
 }
-
-// ㅁ..ㅗ..ㄹ..ㅡ..ㄱ..ㅔ..ㅆ..ㄷ..ㅏ
-// -^-^r  ~~^_^~~
-
-// 가로 세로 길이 랜덤 돌돌(가로>=세로)
-// 가로 + 가로 + (세로-1) + (세로-1) = 갈색 수
