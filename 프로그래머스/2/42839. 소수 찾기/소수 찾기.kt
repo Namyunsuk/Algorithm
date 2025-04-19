3e211a9cf5feb4b2aacbc3cfcd0b5617047d4d71
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2025-04-20 02:24:57 +0900

    [level 2] Title: 소수 찾기, Time: 45.12 ms, Memory: 68.1 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.kt
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.kt
@@ -1,41 +1,43 @@
-import kotlin.math.*
 import java.util.*
+import kotlin.math.*
 
-val sepNums = mutableListOf<String>()
-val combNums = mutableListOf<Int>()
+val primes = HashSet<Int>()
 val vis = Array(8){false}
 
 class Solution {
     fun solution(numbers: String): Int {
         var answer = 0
         
-        numbers.split("").filter{!it.isEmpty()}.forEach{
-            sepNums.add(it)
-        }
+        val nums = numbers.split("").filter{it.isNotBlank()}.map{it.toInt()}
         
-        dfs(sepNums, "")
+        dfs(nums, 0, nums.size)
         
-        answer = combNums.distinct().filter{isPrime(it)}.size
+        answer = primes.size
         
         return answer
     }
-    
-    fun dfs(numbers:MutableList<String>, combNum:String){
-        if(!combNum.isEmpty()) combNums.add(combNum.toInt())
-        for(i in numbers.indices){
-            if(vis[i]) continue
-            vis[i] = true
-            dfs(numbers, combNum+numbers[i])
-            vis[i] = false
-        }
+}
+
+fun dfs(nums:List<Int>, number:Int, n:Int){
+    if(isPrime(number)){
+        primes.add(number)
     }
     
-    fun isPrime(num:Int):Boolean{
-        if(num==0|| num==1) return false
-        if(num==2) return true
-        for(i in 2 .. sqrt(num.toDouble()).toInt()){
-            if(num % i == 0) return false
-        }
-        return true
+    for(i in 0 until n){
+        if(vis[i]) continue
+        vis[i] = true
+        val num = nums[i]
+        dfs(nums, number*10+num, n)
+        vis[i] = false
     }
+}
+
+fun isPrime(num:Int):Boolean{
+    if(num<=1) return false
+    if(num==2) return true
+    val end = sqrt(num.toDouble()).toInt()
+    for(i in 2..end){
+        if(num%i==0) return false
+    }
+    return true
 }
