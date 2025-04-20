04e6403dc7507c06a2e20eb295e1919a3eb14b61
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2025-04-21 03:48:26 +0900

    [Silver II] Title: 랜선 자르기, Time: 192 ms, Memory: 23940 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.kt
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.kt
@@ -4,28 +4,27 @@ val pipes = mutableListOf<Int>()
 
 fun main(): Unit = with(System.`in`.bufferedReader()) {
     val (k, n) = readLine().split(" ").map { it.toInt() }
-    repeat(k) {
+    for (i in 0 until k) {
         pipes.add(readLine().toInt())
     }
 
-    pipes.sort()
-    
-    var st = 0L
-    var en = Int.MAX_VALUE.toLong()
 
-    while (st < en) {
-        val mid = (st + en + 1) / 2
-        if (isValid(pipes, mid.toInt(), n)) st = mid
-        else en = mid - 1
+    var s = 1L
+    var e = ((2.0).pow(31) - 1).toLong()
+
+    while (s < e) {
+        val mid = (s + e + 1) / 2
+        if (isValid(mid.toInt(), n)) s = mid
+        else e = mid - 1
     }
 
-    print(st)
+    print(s)
 }
 
-fun isValid(arr: List<Int>, length: Int, n: Int): Boolean {
+fun isValid(len: Int, n: Int): Boolean {
     var cnt = 0
-    arr.forEach {
-        cnt += (it / length)
+    pipes.forEach {
+        cnt += (it / len)
     }
 
     return cnt >= n
