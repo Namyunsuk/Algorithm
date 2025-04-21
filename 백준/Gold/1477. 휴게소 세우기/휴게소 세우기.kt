36b3964b37e0d491ea545aa7e97880b4ff8c5187
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2025-04-22 03:00:21 +0900

    [Gold IV] Title: 휴게소 세우기, Time: 112 ms, Memory: 21328 KB -BaekjoonHub

--- 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.kt
+++ 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.kt
@@ -1,39 +1,34 @@
 val rests = mutableListOf<Int>()
 
-
 fun main(): Unit = with(System.`in`.bufferedReader()) {
     val (n, m, l) = readLine().split(" ").map { it.toInt() }
-    if (n != 0) {
-        rests.addAll(readLine().split(" ").map { it.toInt() })
-    }
-    rests.add(l)
+    if(n>0)rests.addAll(readLine().split(" ").map { it.toInt() })
+
+    rests.add(0)
     rests.sort()
+    rests.add(l)
+
 
     var s = 1
-    var e = l - 1
+    var e = l
 
     while (s < e) {
         val mid = (s + e) / 2
-        if (isValid(rests, m, mid)) e = mid
+        if (isValid(m, mid)) e = mid
         else s = mid + 1
     }
-
     print(s)
 }
 
-fun isValid(arr: List<Int>, m: Int, dist: Int): Boolean {
-    var cur = 0
+fun isValid(m: Int, minSpace: Int): Boolean {
     var cnt = 0
-    var idx = 0
 
-    while (idx < arr.size) {
-        if (arr[idx] - cur > dist) {
-            cur += dist
-            if (cnt == m) return false
-            cnt++
-        } else {
-            cur = arr[idx++]
-        }
+    for (i in 0 until rests.size - 1) {
+        val space = rests[i + 1] - rests[i]
+        if (space <= minSpace) continue
+        val needRests = if (space % minSpace == 0) (space / minSpace) - 1 else space / minSpace
+        cnt += needRests
+        if (cnt > m) return false
     }
 
     return true
