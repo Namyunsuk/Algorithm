90ff2f7d44fadf96562b6112f511981ebb3ae954
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2025-04-23 12:16:40 +0900

    [Gold V] Title: 평범한 배낭, Time: 360 ms, Memory: 207860 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.kt
@@ -0,0 +1,25 @@
+class Equip(val weight: Int, val v: Int)
+
+val equips = mutableListOf<Equip>()
+val dp = Array(102) { Array(100002) { 0 } }
+
+fun main(): Unit = with(System.`in`.bufferedReader()) {
+    val (n, k) = readLine().split(" ").map { it.toInt() }
+    equips.add(Equip(0, 0))
+    repeat(n) {
+        val (w, v) = readLine().split(" ").map { it.toInt() }
+        equips.add(Equip(w, v))
+    }
+
+    for (i in 1..n) {
+        for (w in 1..k) {
+            val equip = equips[i]
+            if (equip.weight > w) dp[i][w] = dp[i - 1][w]
+            else {
+                dp[i][w] = maxOf(dp[i - 1][w], dp[i - 1][w - equip.weight] + equip.v)
+            }
+        }
+    }
+
+    print(dp[n][k])
+}
