5140040af011587f67eee5bd99e39f39267e2254
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2025-06-28 20:05:11 +0900

    [level 3] Title: n + 1 카드게임, Time: 33.34 ms, Memory: 64.5 MB -BaekjoonHub

--- 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.kt
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.kt
@@ -1,73 +1,80 @@
-var round = 1
-val hands = mutableListOf<Int>()
-val draw = mutableListOf<Int>()
+var hands = mutableListOf<Int>()
+var back = mutableListOf<Int>()
+var coins = 0
 
 class Solution {
     fun solution(coin: Int, cards: IntArray): Int {
+        var answer: Int = 1
         val n = cards.size
-        var coins = coin
-        for(i in 0 until n/3){
+        coins = coin
+        
+        for(i in 0 until cards.size/3){
             hands.add(cards[i])
         }
-        var idx = n/3
-        while(true){
-            if(idx>=n) break
+        
+        var i = cards.size/3
+        
+        while(i < cards.size){
+            var flag = false
+            hands.sort()
             
-            draw.add(cards[idx++])
-            draw.add(cards[idx++])
+            // 카드 뽑기
+            val card1 = cards[i++]
+            val card2 = cards[i++]
+            back.add(card1)
+            back.add(card2)
+            back.sort()
+
             
-            if(canGo(n,hands)){
-                round++
-                continue
-            }
-            
-            if(coins>=1){
-                var flag = false
-                for(card in hands){
-                    if(draw.contains(n+1-card)){
-                        hands.remove(card)
-                        draw.remove(n+1-card)
-                        coins--
-                        round++
-                        flag=true
-                        break
-                    }
+            // 가진 카드에서 해결            
+            for(handCard in hands){
+                val target = n+1-handCard
+                if(hands.binarySearch(target)>=0){
+                    hands.remove(handCard)
+                    hands.remove(target)
+                    answer++
+                    flag = true
+                    break
                 }
-                if(flag) continue
             }
             
-            if(coins>=2 && canGo(n,draw)){
-                coins-=2
-                round++
-            }else{
-                break
+            if(flag) continue
+            
+            // 1장은 보유 + 1장은 뽑은 카드1
+            for(handCard in hands){
+                if(coins<1) break
+                val target = n+1-handCard
+                if(back.binarySearch(target)>=0){
+                    hands.remove(handCard)
+                    back.remove(target)
+                    answer++
+                    flag = true
+                    coins--
+                    break
+                }
             }
+            
+            if(flag) continue
+            
+            // 2장 다 Back에서
+            for(backCard in back){
+                if(coins<2) break
+                val target = n+1-backCard
+                if(back.binarySearch(target)>=0){
+                    back.remove(backCard)
+                    back.remove(target)
+                    answer++
+                    coins-=2
+                    flag = true
+                    break
+                }
+            }                
+        
+            
+            if(!flag) break
         }
         
-        return round
+        
+        return answer
     }
 }
-
-
-fun canGo(n:Int, hands:MutableList<Int>):Boolean{
-    for(card in hands){
-        if(hands.contains(n+1-card)){
-            hands.remove(card)
-            hands.remove(n+1-card)
-            return true
-        }
-    }
-    return false
-}
-
-
-
-
-
-
-
-
-
-
-
-
