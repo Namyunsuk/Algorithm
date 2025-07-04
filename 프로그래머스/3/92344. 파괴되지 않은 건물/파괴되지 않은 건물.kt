68d82479285950fe0057575068ba48e433e68641
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2025-07-04 18:30:23 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 222.99 ms, Memory: 233 MB -BaekjoonHub

---
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.kt
@@ -0,0 +1,48 @@
+val graph = Array(1003){Array(1003){0}}
+
+class Solution {
+    fun solution(board: Array<IntArray>, skill: Array<IntArray>): Int {
+        var answer: Int = 0
+        
+        val n = board.size
+        val m = board[0].size
+        
+        
+        
+        for(s in skill){
+            val type = s[0]
+            val r1 = s[1]
+            val c1 = s[2]
+            val r2 = s[3]
+            val c2 = s[4]
+            var degree = if(type==1) -s[5] else s[5]
+            
+            graph[r1][c1] += degree
+            graph[r1][c2+1] += -degree
+            graph[r2+1][c1] += -degree
+            graph[r2+1][c2+1] += degree
+        }
+        
+        // 좌 -> 우
+        for(i in 0 until n){
+            for(j in 1 until m){
+                graph[i][j] +=graph[i][j-1]
+            }
+        }
+        
+        // 상 -> 하
+        for(i in 1 until n){
+            for(j in 0 until m){
+                graph[i][j] +=graph[i-1][j]
+            }
+        }
+        
+        for(i in 0 until n){
+            for(j in 0 until m){
+                if(graph[i][j] + board[i][j] > 0) answer++
+            }
+        }
+        
+        return answer
+    }
+}
