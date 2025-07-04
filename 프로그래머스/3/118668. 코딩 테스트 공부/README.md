90da2c1302c7844013db09e096cf22027ddb77c6
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2025-07-04 17:02:24 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 59.06 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/README.md
+++ 프로그래머스/3/118668. 코딩 테스트 공부/README.md
@@ -1,10 +1,10 @@
 # [level 3] 코딩 테스트 공부 - 118668 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118668#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118668) 
 
 ### 성능 요약
 
-메모리: 61.1 MB, 시간: 51.54 ms
+메모리: 62.4 MB, 시간: 59.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 20일 00:18:24
+2025년 07월 04일 17:02:21
 
 ### 문제 설명
 
--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.kt
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.kt
@@ -1,42 +1,52 @@
-val dp = Array(151){Array(151){Int.MAX_VALUE}}
+val dp = Array(304){Array(304){Int.MAX_VALUE}}
 
 class Solution {
     fun solution(alp: Int, cop: Int, problems: Array<IntArray>): Int {
         var answer: Int = 0
-        var maxAlp = Int.MIN_VALUE
-        var maxCop = Int.MIN_VALUE
         
-        problems.forEach{
-            maxAlp = maxOf(maxAlp, it[0])
-            maxCop = maxOf(maxCop, it[1])
-        }
-        
-        maxAlp = maxOf(maxAlp, alp)
-        maxCop = maxOf(maxCop, cop)
-        
-        dp[alp][cop] = 0
-        
-        
-        for(i in alp .. maxAlp){
-            for(j in cop .. maxCop){
-                if(i+1<=maxAlp){
-                    dp[i+1][j] = minOf(dp[i+1][j], dp[i][j] + 1)   
-                }
-                if(j+1<=maxCop){
-                    dp[i][j+1] = minOf(dp[i][j+1], dp[i][j] + 1)   
-                }
-                
-                for((alp_req, cop_req, alp_rwd, cop_rwd, cost) in problems){
-                    if(i>=alp_req&&j>=cop_req){
-                        val newI = minOf(maxAlp, i+alp_rwd)
-                        val newJ = minOf(maxCop, j+cop_rwd)
-                        dp[newI][newJ] = minOf(dp[newI][newJ], dp[i][j] + cost)
-                    }
-                }
+        for(i in 0 .. alp){
+            for(j in 0 .. cop){
+                dp[i][j] = 0
             }
         }
         
+        var targetAlp = alp
+        var targetCop = cop
         
-        return dp[maxAlp][maxCop]
+        problems.forEach{
+            val ra = it[0]
+            val rc = it[1]
+            
+            targetAlp = maxOf(targetAlp, ra)
+            targetCop = maxOf(targetCop, rc)
+        }
+        
+        for(i in alp .. targetAlp){
+            for(j in cop .. targetCop){
+                if(i==targetAlp && j==targetCop) break
+                // 문제  풀이
+                for(problem in problems){
+                    val alp_req = problem[0]
+                    val cop_req = problem[1]
+                    val alp_rwd = problem[2]
+                    val cop_rwd = problem[3]
+                    val cost = problem[4]
+                    
+                    if(i < alp_req || j < cop_req ) continue
+                    val a = minOf(i+alp_rwd, targetAlp)
+                    val c = minOf(j+cop_rwd, targetCop)
+                    
+                    dp[a][c] = minOf(dp[a][c], dp[i][j] + cost)
+                }
+                
+                // 알고력 상승
+                dp[i+1][j] = minOf(dp[i+1][j], dp[i][j] + 1)
+                
+                // 코딩력 상승
+                dp[i][j+1] = minOf(dp[i][j+1], dp[i][j] + 1)
+            }
+        }
+        
+        return dp[targetAlp][targetCop]
     }
 }
