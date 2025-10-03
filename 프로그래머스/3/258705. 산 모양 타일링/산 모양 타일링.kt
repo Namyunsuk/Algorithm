7e177ab48dbd3775e82b02374ec74d1009413b2b
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2025-10-04 01:38:35 +0900

    [level 3] Title: 산 모양 타일링, Time: 49.14 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.kt
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.kt
@@ -1,18 +1,24 @@
-val dp = Array(2*100_000+2){0}
+val dp = Array(200_002){0}
 
 class Solution {
     fun solution(n: Int, tops: IntArray): Int {
         var answer: Int = 0
         
         dp[0] = 1
-        dp[1] = 1
+        if(tops[0]==1) dp[1] = 3
+        else dp[1] = 2
         
-        for(i in 2..2*n+1){
+        for(i in 2 until 2*n+1){
             dp[i] = dp[i-1] + dp[i-2]
-            if(i%2==0 && tops[i/2 - 1] == 1) dp[i]+=dp[i-1]
-            dp[i] %=10007
+            
+            if(i%2==1){
+                val j = (i-1)/2
+                if(tops[j]==1) dp[i]+=dp[i-1]
+            }
+            
+            dp[i]%=10_007
         }
         
-        return dp[2*n+1]
+        return dp[2*n]
     }
 }
