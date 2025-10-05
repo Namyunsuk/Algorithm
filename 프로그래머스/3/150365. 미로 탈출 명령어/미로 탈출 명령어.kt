c125f3122975aad5542708fcbf941e9feaed031a
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2025-10-06 00:33:04 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 1382.09 ms, Memory: 439 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.kt
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.kt
@@ -1,12 +1,11 @@
 import java.util.*
 
-class Pos(val x:Int, val y:Int, val path:String="", val dist:Int=0)
+class Pos(val x:Int, val y:Int, val path:String = "")
 
-val vis = Array(52){Array(52){Array(2502){false}}}
-val dx = listOf(1, 0, 0, -1)
-val dy = listOf(0, -1, 1, 0)
-
+val vis = Array(2501){Array(51){Array(51){false}}}
 val q = LinkedList<Pos>()
+val dx = listOf(1,0,0,-1)
+val dy = listOf(0,-1,1,0)
 
 class Solution {
     fun solution(n: Int, m: Int, x: Int, y: Int, r: Int, c: Int, k: Int): String {
@@ -17,31 +16,39 @@ class Solution {
         while(!q.isEmpty()){
             val cur = q.poll()
             
-            for(i in 0 .. 3){
+            for(i in 0 until 4){
                 val nx = cur.x + dx[i]
                 val ny = cur.y + dy[i]
-                val dist = cur.dist + 1
-                val path = cur.path + dToS(dx[i], dy[i])
+                val dist = cur.path.length + 1
                 
-                if(nx<1 || nx > n || ny < 1 || ny > m) continue
-                if(dist>k) continue
-                if(vis[nx][ny][dist]) continue
-                if(nx == r && ny == c && dist == k) return path
+                if(nx<1 || nx>n || ny<1 || ny>m) continue
+                if(dist > k) continue
+                if(vis[dist][nx][ny]) continue
+                if(dist == k){
+                    if(nx == r && ny == c){
+                        return cur.path + dirToString(dx[i], dy[i])                    
+                    }
+                    continue
+                }
                 
-                vis[nx][ny][dist] = true
-                q.offer(Pos(nx, ny, path, dist))
+                val newPath = cur.path + dirToString(dx[i], dy[i])
+                
+                q.offer(Pos(nx, ny, newPath))
+                vis[dist][nx][ny] = true
             }
         }
-        
         
         return "impossible"
     }
 }
 
-fun dToS(dx:Int, dy:Int):String{
-    if(dx == 1 && dy == 0) return "d"
-    else if(dx == 0 && dy == -1) return "l"
-    else if(dx == 0 && dy == 1) return "r"
-    
+fun dirToString(dx:Int, dy:Int):String{
+    if(dx==1&&dy==0){
+        return "d"
+    } else if(dx==0&&dy==-1){
+        return "l"
+    } else if(dx==0&&dy==1){
+        return "r"
+    }
     return "u"
 }
