c04efed4eaee49dfbacfae5eb12c849f543e6b65
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2025-10-07 01:08:20 +0900

    [level 3] Title: 양과 늑대, Time: 3.90 ms, Memory: 64.5 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.kt
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.kt
@@ -1,3 +1,9 @@
+/**
+참고 링크
+
+https://velog.io/@thguss/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-L3-%EC%96%91%EA%B3%BC-%EB%8A%91%EB%8C%80-python
+
+**/ 
 val vis = Array(19){false}
 var answer: Int = 0
 
@@ -29,7 +35,3 @@ class Solution {
     }
     
 }
-
-
-
-
