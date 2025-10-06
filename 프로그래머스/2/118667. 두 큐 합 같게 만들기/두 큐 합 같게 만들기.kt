146d2888559fe7ecb018718f6240b206f8d6c342
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2025-10-06 22:50:20 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 132.04 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.kt
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.kt
@@ -1,43 +1,44 @@
 import java.util.*
-import kotlin.math.*
-
-val q1 = LinkedList<Long>()
-val q2 = LinkedList<Long>()
 
 class Solution {
     fun solution(queue1: IntArray, queue2: IntArray): Int {
         var answer: Int = 0
-        val limitSize = queue1.size * 4
         
-        queue1.forEach{
-            q1.offer(it.toLong())
-        }
+        val q1 = LinkedList<Long>()
+        val q2 = LinkedList<Long>()
         
-        queue2.forEach{
-            q2.offer(it.toLong())
-        }
+        
+        queue1.forEach{q1.offer(it.toLong())}
+        queue2.forEach{q2.offer(it.toLong())}
+        
+        
+        val limit = 4 * q1.size
+        
         
         var q1Sum = q1.sum()
         var q2Sum = q2.sum()
         
-        while(q1Sum != q2Sum){
-            if(answer>limitSize) return -1
+        
+        while(q1Sum!= q2Sum){
+            if(answer > limit) return -1
+            
             if(q1Sum > q2Sum){
-                q1Sum-=q1.peek()
-                q2Sum+=q1.peek()
-                q2.offer(q1.poll())
-                answer++
-                continue
+                val e = q1.poll()
+                q1Sum -=e
+                
+                q2.offer(e)
+                q2Sum +=e
+            }else{
+                val e = q2.poll()
+                q2Sum -=e
+                
+                q1.offer(e)
+                q1Sum +=e
             }
-            q2Sum-=q2.peek()
-            q1Sum+=q2.peek()
-            q1.offer(q2.poll())
+            
             answer++
         }
         
         return answer
     }
 }
-
-// 큰 -> 작으로 넣어줌
-
