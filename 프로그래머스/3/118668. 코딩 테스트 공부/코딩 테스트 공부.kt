fc8bea745dd899900a342f3fe90d61bd001e264d
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2025-10-06 22:19:28 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 47.75 ms, Memory: 60.5 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.kt
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.kt
@@ -1,8 +1,19 @@
-val dp = Array(304){Array(304){Int.MAX_VALUE}}
+val dp = Array(152){Array(152){Int.MAX_VALUE}}
 
 class Solution {
     fun solution(alp: Int, cop: Int, problems: Array<IntArray>): Int {
         var answer: Int = 0
+        
+        var maxAlp = alp
+        var maxCop = cop
+        
+        problems.forEach{ problem->
+            val alp_req = problem[0]
+            val cop_req = problem[1]
+            
+            maxAlp = maxOf(alp_req, maxAlp)
+            maxCop = maxOf(cop_req, maxCop)
+        }
         
         for(i in 0 .. alp){
             for(j in 0 .. cop){
@@ -10,43 +21,31 @@ class Solution {
             }
         }
         
-        var targetAlp = alp
-        var targetCop = cop
-        
-        problems.forEach{
-            val ra = it[0]
-            val rc = it[1]
-            
-            targetAlp = maxOf(targetAlp, ra)
-            targetCop = maxOf(targetCop, rc)
-        }
-        
-        for(i in alp .. targetAlp){
-            for(j in cop .. targetCop){
-                if(i==targetAlp && j==targetCop) break
-                // 문제  풀이
+        for(i in alp .. maxAlp){
+            for(j in cop .. maxCop){
+                dp[i+1][j] = minOf(dp[i+1][j], dp[i][j] + 1)
+                dp[i][j+1] = minOf(dp[i][j+1], dp[i][j] + 1)
+                
                 for(problem in problems){
                     val alp_req = problem[0]
                     val cop_req = problem[1]
+                    
                     val alp_rwd = problem[2]
                     val cop_rwd = problem[3]
+                    
                     val cost = problem[4]
                     
-                    if(i < alp_req || j < cop_req ) continue
-                    val a = minOf(i+alp_rwd, targetAlp)
-                    val c = minOf(j+cop_rwd, targetCop)
-                    
-                    dp[a][c] = minOf(dp[a][c], dp[i][j] + cost)
+                    if(i >= alp_req && j >= cop_req){
+                        val totalAlp = minOf(i+alp_rwd, maxAlp)
+                        val totalCop = minOf(j+cop_rwd, maxCop)
+                        
+                        dp[totalAlp][totalCop] = minOf(dp[totalAlp][totalCop], dp[i][j] + cost)
+                    }
                 }
-                
-                // 알고력 상승
-                dp[i+1][j] = minOf(dp[i+1][j], dp[i][j] + 1)
-                
-                // 코딩력 상승
-                dp[i][j+1] = minOf(dp[i][j+1], dp[i][j] + 1)
             }
         }
         
-        return dp[targetAlp][targetCop]
+        
+        return dp[maxAlp][maxCop]
     }
 }
