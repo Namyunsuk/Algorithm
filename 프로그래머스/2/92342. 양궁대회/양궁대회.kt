109a6e742692af7ab3ebf906501b14023e9e4a9c
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2025-10-07 20:57:31 +0900

    [level 2] Title: 양궁대회, Time: 204.65 ms, Memory: 64 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.kt
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.kt
@@ -1,71 +1,81 @@
-import kotlin.math.*
-
-var canRyanWin = false
-var winningInfo = mutableListOf<Int>()
-var maxDiff = 0
+var answer = mutableListOf(-1)
+var diff = 0
+var canWin = false
 
 class Solution {
     fun solution(n: Int, info: IntArray): IntArray {
-        var answer = mutableListOf<Int>()
         
-        dfs(n,mutableListOf<Int>(), info)
+        dfs(0, mutableListOf(0,0,0,0,0,0,0,0,0,0,0), n, info)
         
-        println(maxDiff)
+        return answer.toIntArray()
+    }
+}
+
+fun dfs(cnt:Int, selected:MutableList<Int>, n: Int, info: IntArray){
+    if(cnt == n){
         
-        if(!canRyanWin){
-            answer.add(-1)
-            return answer.toIntArray()
+        var aSum = 0
+        var lSum = 0
+        for(i in 0..10){
+            val score = 10 - i
+            val apeach = info[i]
+            val lion = selected[i]
+            
+            if(apeach == 0 && lion == 0) continue
+            else if(apeach >= lion) aSum += score
+            else lSum += score
         }
         
-        winningInfo.reverse()
-        
-        return winningInfo.toIntArray()
-    }
-    
-    fun dfs(remainArrowCount:Int, ryanInfo:MutableList<Int>, info: IntArray){
-        if(ryanInfo.size == 11){
-            var apeach = 0
-            var ryan = 0
-            
-            for(i in 0..10){
-                if(ryanInfo[i]==0 && info[10-i]==0) continue
-                else if(ryanInfo[i] > info[10-i]) ryan+=i
-                else apeach+=i
+        if(lSum > aSum){
+            // 더 큰 차이인 경우
+            if(lSum - aSum > diff){
+                answer.clear()
+                answer.addAll(selected.toList())
+                diff = lSum - aSum
             }
-            
-            if(ryan>apeach){
-                canRyanWin = true
-                val diff = ryan - apeach
-                if(diff>maxDiff){
-                    maxDiff = diff
-                    winningInfo.clear()
-                    winningInfo.addAll(ryanInfo.toList())
-                }else if(diff == maxDiff){
-                    for(i in 0..10){
-                        if(ryanInfo[i] > winningInfo[i]){
-                            winningInfo.clear()
-                            winningInfo.addAll(ryanInfo.toList())
-                            return
-                        }
+            // 차이가 같을 경우
+            else if(lSum - aSum == diff){
+                for(i in 10 .. 0){
+                    if(selected[i] > answer[i]) {
+                        answer.clear()
+                        answer.addAll(selected.toList())
+                        break
                     }
                 }
             }
-            
-            return
         }
-        
-        if(ryanInfo.size == 10){
-            ryanInfo.add(remainArrowCount)
-            dfs(0, ryanInfo, info)
-            ryanInfo.removeAt(ryanInfo.size - 1)
-            
-            return
-        }
-        
-        for(i in 0..remainArrowCount){
-            ryanInfo.add(i)
-            dfs(remainArrowCount - i, ryanInfo, info)
-            ryanInfo.removeAt(ryanInfo.size - 1)
-        }
+        return
     }
+    // [1,1,2,3,0,2,0,0,0,0,0]
+    // [1,1,2,0,1,2,2,0,0,0,0]
+    
+    val remainArrow = n - cnt
+    
+    for(score in 0 .. 10){
+        val i = 10 - score
+        
+        // 해당 점수를 어피치보다 많이 쏜 경우 또 쏠필요 X
+        if(selected[i] > info[i]) continue
+        // 해당 점수를 얻을 화살 수가 여유 있을 경우
+        if(remainArrow > info[i]){
+            selected[i] += info[i] + 1
+
+            dfs(cnt + info[i] + 1, selected, n, info)
+
+            selected[i] -= info[i] + 1
+        }
+        
+        // 쏠 곳이 없는 경우
+        if(i == 10){
+            selected[10] += remainArrow
+            
+            dfs(cnt + remainArrow, selected, n , info)
+
+            selected[10] -= remainArrow
+        }
+    }   
 }
+
+// info => 10, 9, 8 .. 점수 순임
+// 점수차이 같을 경우 => 낮은 점수를 더 많이 맞힌 순서
+// 이길 방법 없을 경우 -1
