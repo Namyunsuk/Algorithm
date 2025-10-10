5d165159fa375a2d05fb047ee37df2d7e5fb67fa
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2025-10-10 14:06:57 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 79.54 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.kt
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.kt
@@ -1,66 +1,54 @@
-var maxCnt = 0
-var maxSelectedMenu = mutableListOf<String>()
+val cases = Array(15){mutableSetOf<String>()}
+val orderMap = HashMap<String, Int>()
 
 class Solution {
     fun solution(orders: Array<String>, course: IntArray): Array<String> {
-        val answer = mutableListOf<String>()
+        var answer = mutableListOf<String>()
         
-//         for(order in orders){
-//             val splittedOrder = order.split("").filter{it.isNotBlank()}
-//             menus.addAll(splittedOrder)
-//         }
-//         menus.sort()
-//         menus = menus.distinct().toMutableList()
+        orders.forEach{
+            val split = it.split("").filter{it.isNotBlank()}.toMutableList()
+            val order = split.sorted().joinToString("")
+            dfs(0, "", order)
+        }
         
-        
-        for(c in course){
-            maxCnt = 0
-            maxSelectedMenu = mutableListOf<String>()
-            
-            for(order in orders){
-                val menus = order.split("").filter{it.isNotBlank()}
-                dfs(0,0,c,mutableListOf(), orders, menus.sorted())    
+        course.forEach{ cnt->
+            var order = mutableListOf<String>()
+            var count = 0
+            cases[cnt].forEach{
+                if(orderMap[it]!! >=2){
+                    if(orderMap[it]!! > count){
+                        order.clear()
+                        order.add(it)
+                        count = orderMap[it]!!
+                    }else if(orderMap[it]!! == count){
+                        order.add(it)
+                    }
+                }
             }
-            
-            
-            answer.addAll(maxSelectedMenu.toList())
+            answer.addAll(order.toList())
         }
         
         answer.sort()
         
-        return answer.distinct().toTypedArray()
+        return answer.toTypedArray()
     }
     
-    fun dfs(index:Int, cnt:Int, target:Int,selected:MutableList<String>, orders: Array<String>, menus:List<String>){
-        if(cnt == target){
-            val orderCnt = calCnt(selected, orders)
-            if(orderCnt<2) return
-            if(orderCnt > maxCnt){
-                maxCnt = orderCnt
-                maxSelectedMenu.clear()
-                maxSelectedMenu.add(selected.joinToString(""))
-            }else if(orderCnt == maxCnt){
-                maxSelectedMenu.add(selected.joinToString(""))
-            }
-            
-            return
+    fun dfs(idx:Int, selected:String, order:String){
+        if(selected.length >= 2){
+            cases[selected.length].add(selected)
+            if(orderMap[selected] == null) orderMap[selected] = 1
+            else orderMap[selected] = orderMap[selected]!! + 1
         }
         
-        for(i in index until menus.size){
-            selected.add(menus[i])
-            dfs(i+1, cnt+1,target, selected, orders, menus)
-            selected.remove(menus[i])
-        }
-    }
-    
-    fun calCnt(selected:List<String>, orders: Array<String>):Int{
-        var cnt = 0
+        if(selected.length == order.length) return
         
-        for(order in orders){
-            val splittedOrder = order.split("").filter{it.isNotBlank()}
-            if(splittedOrder.containsAll(selected)) cnt++
+        for(i in idx until order.length){
+            dfs(i+1, selected + order[i].toString(), order)
         }
-        
-        return cnt
     }
 }
+
+
+
+
+
