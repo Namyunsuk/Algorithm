c597b05256c80482c02f7305870c3e2c09f8fe41
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2025-10-10 21:48:21 +0900

    [level 2] Title: 문자열 압축, Time: 28.19 ms, Memory: 69.3 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.kt
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.kt
@@ -1,36 +1,67 @@
-import kotlin.math.*
-
 class Solution {
     fun solution(s: String): Int {
         var answer = Int.MAX_VALUE
         
-        for(unit in 1..s.length){
-            var i = 0
-            var str = ""
-            var unitStr = ""
-            var cnt = 1
-            
-            while(i<s.length){
-                unitStr = if(i+unit<s.length) s.substring(i,i+unit) else s.substring(i,s.length)
-                while(true){
-                    if(i+unit>=s.length || i+2*unit > s.length) break
-                    var nextStr = s.substring(i+unit, i+2*unit)
-                    if(unitStr!=nextStr) break
-                    cnt++
-                    i+=unit
-                }
-                if(cnt!=1){
-                    str+=cnt.toString()
-                }
-                cnt=1
-                str+=unitStr
-                
-                i+=unit
-                
-            }
-            answer = min(str.length, answer)
+        val n = s.length
+        
+        if(n==1) return 1
+        
+        for(unit in 1 .. (n/2)){
+            val len = solve(unit, s)
+            if(len < answer) answer = len
         }
         
         return answer
     }
+    
+    fun solve(unit:Int, str:String):Int{
+        var newStr = ""
+        var cnt = 1
+        var before = ""
+        
+        for(i in 0 until str.length step unit){
+            if(i + unit > str.length) break
+            val cur = str.substring(i, i+unit)
+            if(i==0){
+                before = cur
+                continue
+            }
+            
+            if(before == cur) cnt++
+            else{
+                if(cnt==1){
+                    newStr+=before
+                }else{
+                    newStr+=(cnt.toString()) + before
+                }
+                
+                before = cur
+                cnt = 1
+            }
+        }
+        
+        if(cnt==1){
+            newStr+=before
+        }else{
+            newStr+=(cnt.toString()) + before
+        }
+        
+        // 자투리 계산
+        val r = str.length % unit
+        if(r!=0){
+            val restStr = str.substring(str.length - r ,str.length)   
+            newStr+=restStr
+        }
+        
+        return newStr.length
+    }
 }
+
+
+
+
+
+
+
+
+
